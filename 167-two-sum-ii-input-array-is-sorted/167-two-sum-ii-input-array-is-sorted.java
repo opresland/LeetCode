48726830687f7d1c8a38e2b891adfd98a2f8dac0
opresland <91330166+opresland@users.noreply.github.com>
2022-09-04 17:43:25 +0100

    Time: 3 ms (39.31%), Space: 50.2 MB (35.10%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -4,11 +4,8 @@ class Solution {
         
         //selecting largest poss. number
         for(int i=numbers.length-1; i>0; i--){
-            System.out.println("i is "+i);
-            //if(numbers[i]<target){
                 //selecting smallest possible
                 for(int j=0; j<i; j++){
-                    System.out.println("j is "+j);
                     if(numbers[i]+numbers[j] > target){
                         break;
                     }else if(numbers[i]+numbers[j] == target){
@@ -16,8 +13,6 @@ class Solution {
                         return result;
                     }
                 }
-                
-            //}
         }
         
         return new int[2];
